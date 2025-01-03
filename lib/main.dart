45d9a53c239e758bc9ebcc41d03addf0e2077564
utpal <utpalvir98@gmail.com>
2025-01-03 05:31:16 +0530

    playing With themes

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,22 @@ class BMIApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF0A0E21),
+          foregroundColor: Colors.white, // Custom elevation
+        ),
+        textTheme: TextTheme(
+          headlineLarge: TextStyle(color: Colors.white),
+        ),
+      ),
       home: InputPage(),
+      // theme: ThemeData(
+      //   colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.purple),
+      // ),
     );
   }
 }
@@ -23,17 +38,20 @@ class _InputPageState extends State<InputPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('BMI Calculator'),
-        titleTextStyle: TextStyle(
-          color: Colors.cyan,
-          fontSize: 24,
-          backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          'BMI CALCULATOR',
+          style: Theme.of(context).textTheme.headlineLarge,
+          // Ensures the title is centered
         ),
       ),
       body: Center(
         child: Text('Hello World'),
       ),
-      backgroundColor: Colors.black87,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
     );
   }
 }
